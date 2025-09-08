789349a5d5ff4a611f71506c9ce7c61dfdc65c96
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-09-08 13:01:17 +0530

    added base-url

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://ai-vent-planner.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
