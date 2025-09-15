73914691977ab1f0081157a38a08857970070ba4
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-09-15 22:05:41 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 const app = express();
 loadInitialData();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://ai-vent-planner-1.onrender.com"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
