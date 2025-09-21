6a384acc7df897d01107cedba49288d452659e2c
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-09-21 13:56:11 +0530

    fix layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
-import { useTheme } from "../context/ThemeContext";  
+import { useTheme } from "../context/ThemeContext";
 
 const Layout = () => {
-  const { currentTheme } = useTheme();  
+  const { currentTheme } = useTheme();
 
   return (
     <div
       className={`flex flex-col min-h-screen ${currentTheme.bg} transition-colors duration-500 overflow-hidden`}
     >
-      {/* Fixed Navbar */}
       <header className="z-50">
         <Navbar />
       </header>
-
-      {/* Main Content */}
-      <main className=" pt-16">
+      <main className="">
         <Outlet />
       </main>
     </div>
